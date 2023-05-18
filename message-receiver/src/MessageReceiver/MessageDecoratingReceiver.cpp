a565629992066672be2c5660c5bc67c6588d3375
krzysztof.obrebski <Krzysztof.Obrebski@GAINCapital.com>
2023-05-18 22:41:48 +0200

    tidy up

--- message-receiver/src/MessageReceiver/MessageDecoratingReceiver.cpp
+++ message-receiver/src/MessageReceiver/MessageDecoratingReceiver.cpp
@@ -10,7 +10,6 @@ MessageDecoratingReceiver::MessageDecoratingReceiver(const MessageDecoratingRece
 
 void MessageDecoratingReceiver::onMessage(const cms::Message* message)
 {
-	std::cout<<"received message - RO DO implement parser and decorator"<<std::endl;
 	verify(message);
 	if (mListener)
 		mListener->onMessage(message);
